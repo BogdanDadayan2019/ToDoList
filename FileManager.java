d5eaf26613f8556d82a5f2f7f510c1977b439cf4
Bogdan Dadaian <bo.dadaian@tnnk.local>
2025-01-14 11:52:24 +0100

    init file manager

---
+++ FileManager.java
@@ -0,0 +1,42 @@
+package toDo;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileManager {
+	
+	static String filePath = "tasks.txt";
+	// Methode zum Laden der Aufgaben aus der Datei
+    public static void loadTasksFromFile() {
+        System.out.println("--- Versuche, Aufgaben aus der Datei zu laden...");
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] taskParts = line.split(":", 2);
+                int id = Integer.parseInt(taskParts[0]);
+                String text = taskParts[1];
+                ToDo.tasks.add(new Task(id, text));
+                ToDo.idTaskEncounter = Math.max(ToDo.idTaskEncounter, id + 1); 
+            }
+        } catch (IOException e) {
+        	ToDo.displayError("Fehler beim Laden der Aufgaben aus der Datei: " + e.getMessage());
+        }
+    }
+
+    // Methode zum Speichern der Aufgaben in die Datei
+    public static void saveTasksToFile() {
+        System.out.println("--- Speichern der Aufgaben in die Datei...");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
+            for (Task task : ToDo.tasks) {
+                writer.write(task.getId() + ":" + task.getText());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+        	ToDo.displayError("Fehler beim Speichern der Aufgaben in die Datei: " + e.getMessage());
+        }
+    }
+
+}
